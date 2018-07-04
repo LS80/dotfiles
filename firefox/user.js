d25bbcb53967eabcf0887d424116ba7ff98b5a3f
Lee Smith <lee@lee-smith.me.uk>
2018-07-05 00:25:54 +0100

    Add Firefox advanced preferences

---
+++ firefox/user.js
@@ -0,0 +1,26 @@
+user_pref("browser.tabs.drawInTitlebar", true);
+user_pref("browser.tabs.loadBookmarksInTabs", true);
+user_pref("browser.showQuitWarning", true);
+user_pref("browser.urlbar.openintab", true);
+user_pref("browser.urlbar.maxRichResults", 20);
+user_pref("browser.urlbar.doubleClickSelectsAll", true);
+user_pref("browser.ctrlTab.previews", true);
+
+user_pref("view_source.editor.external", true);
+user_pref("view_source.editor.path", "/usr/bin/code");
+
+user_pref("layers.acceleration.force-enabled", true);
+user_pref("browser.cache.disk.enable", false);
+user_pref("browser.cache.memory.enable", true);
+
+user_pref("network.dns.disablePrefetch", true);
+user_pref("network.prefetch-next", false);
+user_pref("network.http.speculative-parallel-limit", 0);
+
+user_pref("privacy.trackingprotection.enabled", true);
+user_pref("datareporting.healthreport.service.enabled", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("privacy.resistFingerprinting", true);
+
+user_pref("security.tls.version.min", 3);
